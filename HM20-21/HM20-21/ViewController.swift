b71e4c24dbbd31493b556e14aa9eaa795c8111fd
Vadim <vsadnik199@gmail.com>
2023-09-18 21:36:51 +0300

    project created

---
+++ HM20-21/HM20-21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM20-21
+//
+//  Created by Вадим Игнатенко on 18.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
