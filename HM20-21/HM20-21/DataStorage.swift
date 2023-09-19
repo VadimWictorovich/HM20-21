ce040e8f6ee6205d3e252b722563f7444ae5e8f7
Vadim <vsadnik199@gmail.com>
2023-09-19 11:48:48 +0300

    Commit #1

---
+++ HM20-21/HM20-21/DataStorage.swift
@@ -0,0 +1,53 @@
+//
+//  DataStorage.swift
+//  HM20-21
+//
+//  Created by Вадим Игнатенко on 18.09.23.
+//
+
+import Foundation
+
+struct DataStorage {
+    static let names = ["Ivan",
+                        "Maria",
+                        "Sergey",
+                        "Elena",
+                        "Alexei",
+                        "Olga",
+                        "Dmitri",
+                        "Natalia",
+                        "Pavel",
+                        "Yulia"]
+
+    static let surnames = ["Ivanov",
+                           "Petrov",
+                           "Smirnov",
+                           "Kuznetsov",
+                           "Popov",
+                           "Sokolov",
+                           "Mikhailov",
+                           "Fedorov",
+                           "Morozov",
+                            "Volkov"]
+
+    static let emails = ["coolcat82@gmail.com",
+                         "sunnydayz@gmail.com",
+                         "emailpro123@gmail.com",
+                         "techgeek47@gmail.com",
+                         "musiclover22@gmail.com",
+                         "travelbug77@gmail.com",
+                         "smileyface99@gmail.com",
+                         "soccerfanatic55@gmail.com",
+                         "sbookworm88@gmail.com"]
+
+    static let phones = ["375012345678",
+                         "375123456789",
+                         "375234567890",
+                         "375345678901",
+                         "375456789012",
+                         "375567890123",
+                         "375678901234",
+                         "375789012345",
+                         "375890123456",
+                         "375901234567"]
+}
