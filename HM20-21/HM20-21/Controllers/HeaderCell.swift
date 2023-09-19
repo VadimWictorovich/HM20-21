4cd88c6264bf0fb0c48f6388c85c938558bc2889
Vadim <vsadnik199@gmail.com>
2023-09-19 11:49:18 +0300

    Commit #2

---
+++ HM20-21/HM20-21/Controllers/HeaderCell.swift
@@ -0,0 +1,12 @@
+//
+//  TableViewCell.swift
+//  HM20-21
+//
+//  Created by Вадим Игнатенко on 18.09.23.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+    @IBOutlet var personLabel: UILabel!
+}
